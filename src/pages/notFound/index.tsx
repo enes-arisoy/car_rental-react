677e89500789da2ebe9f8291df03b507391ff4e9
enes-arisoy <enes.arisoy.85@gmail.com>
2025-07-13 14:58:14 +0300

    updated

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,7 +1,12 @@
 import  { type FC } from "react";
+import { Link } from 'react-router-dom';
 
 const NotFound: FC = () => {
-  return <div>404</div>;
+  return <div>
+    <h1>404</h1>
+    <p>Aradığınız içerik bulunamadı.</p>
+    <Link to="/">Anasayfaya Git</Link>
+  </div>;
 };
 
 export default NotFound;
